e5ad51fc4a03804475dff101c215d28bedbd5b17
hellopacman <hellopacman@hotmail.com>
2021-02-27 23:12:41 +0800

    新建项目1.custom_render_pipeline
    1.custom_render_pipeline

---
+++ 1.custom_rp/Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraRenderer
+{
+	ScriptableRenderContext context;
+	Camera camera;
+
+	public void Render(ScriptableRenderContext context, Camera camera)
+	{
+		this.context = context;
+		this.camera = camera;
+
+		DrawVisibleGeometry();
+	}
+
+	void DrawVisibleGeometry()
+	{
+		context.DrawSkybox(camera);
+	}
+
+}
