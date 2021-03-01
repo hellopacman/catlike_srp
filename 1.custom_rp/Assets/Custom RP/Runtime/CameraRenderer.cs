32aa89ea9d483c4d323475a9aebd6a5eb2f6e1ef
hellopacman <hellopacman@hotmail.com>
2021-03-01 21:00:15 +0800

    Update CameraRenderer.cs
    进行中

--- 1.custom_rp/Assets/Custom RP/Runtime/CameraRenderer.cs
+++ 1.custom_rp/Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -3,27 +3,89 @@ using UnityEngine.Rendering;
 
 public class CameraRenderer
 {
+	static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+	
+	private const string bufferName = "Render Camera";
+	CommandBuffer buffer = new CommandBuffer
+	{
+		name = bufferName
+	};
+	
 	ScriptableRenderContext context;
 	Camera camera;
+	CullingResults cullingResults;
 
 	public void Render(ScriptableRenderContext context, Camera camera)
 	{
 		this.context = context;
 		this.camera = camera;
 
+		if (!Cull())
+		{
+			return;
+		}
+			
+		Setup();
 		DrawVisibleGeometry();
-		
 		Submit();
 
 	}
 
 	void DrawVisibleGeometry()
 	{
+		// opaque
+		var sortingSettings = new SortingSettings(camera)
+		{
+			criteria = SortingCriteria.CommonOpaque
+		};
+		var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
+		var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+		context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);		
+		
+		// skybox
 		context.DrawSkybox(camera);
+		
+		// trasparent
+		sortingSettings.criteria = SortingCriteria.CommonTransparent;
+		drawingSettings.sortingSettings = sortingSettings;
+		filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+		context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);		
+		
+	}
+
+	void Setup()
+	{
+		context.SetupCameraProperties(camera);	
+
+		buffer.ClearRenderTarget(true, true, Color.clear);
+
+		buffer.BeginSample(bufferName);
+		ExecuteBuffer();
+		
 	}
 
 	void Submit()
 	{
+		buffer.EndSample(bufferName);
+		ExecuteBuffer();
+
 		context.Submit();
 	}
+	
+	void ExecuteBuffer () {
+		context.ExecuteCommandBuffer(buffer);
+		buffer.Clear();
+	}
+
+	bool Cull ()
+	{
+		if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+		{
+			cullingResults = context.Cull(ref p);
+			return true;
+		}
+		return false;
+	}
+	
+	
 }
