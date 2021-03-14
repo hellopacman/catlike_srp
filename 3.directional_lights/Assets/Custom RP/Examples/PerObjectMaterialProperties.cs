00c1f18ffe74633fe40fc926e613c8e5d7cf5cbe
hellopacman <hellopacman@hotmail.com>
2021-03-14 22:59:54 +0800

    03_direction_lights
    03_direction_lights前半段

---
+++ 3.directional_lights/Assets/Custom RP/Examples/PerObjectMaterialProperties.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class PerObjectMaterialProperties : MonoBehaviour {
+	
+	static MaterialPropertyBlock block;
+
+	static int baseColorId = Shader.PropertyToID("_BaseColor");
+	static int cutoffId = Shader.PropertyToID("_Cutoff");
+
+	[SerializeField]
+    Color baseColor = Color.white;
+    
+    [SerializeField, Range(0f, 1f)]
+    float cutoff = 0.5f;
+    
+    void Awake () {
+	    OnValidate();
+    }
+    
+    void OnValidate () {
+	    if (block == null) {
+		    block = new MaterialPropertyBlock();
+	    }
+	    block.SetColor(baseColorId, baseColor);
+	    block.SetFloat(cutoffId, cutoff);
+	    
+	    GetComponent<Renderer>().SetPropertyBlock(block);
+    }
+}
---
+++ 3.directional_lights/Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public partial class CameraRenderer
+{
+	static ShaderTagId 
+		unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit"),
+		litShaderTagId = new ShaderTagId("CustomLit");
+
+	private const string bufferName = "Render Camera";
+	CommandBuffer buffer = new CommandBuffer
+	{
+		name = bufferName
+	};
+	
+	ScriptableRenderContext context;
+	Camera camera;
+	CullingResults cullingResults;
+	Lighting lighting = new Lighting();
+
+	public void Render(ScriptableRenderContext context, Camera camera,
+		bool useDynamicBatching, bool useGPUInstancing)
+	{
+		this.context = context;
+		this.camera = camera;
+		
+		PrepareBuffer();
+		PrepareForSceneWindow();
+		
+		if (!Cull())
+		{
+			return;
+		}
+			
+		Setup();
+		lighting.Setup(context, cullingResults);
+		DrawVisibleGeometry(useDynamicBatching, useGPUInstancing);
+		DrawUnsupportedShaders();
+		DrawGizmos();
+		Submit();
+	}
+
+	void DrawVisibleGeometry (bool useDynamicBatching, bool useGPUInstancing) {
+		var sortingSettings = new SortingSettings(camera) {
+			criteria = SortingCriteria.CommonOpaque
+		};
+		var drawingSettings = new DrawingSettings(
+			unlitShaderTagId, sortingSettings
+		) {
+			enableDynamicBatching = useDynamicBatching,
+			enableInstancing = useGPUInstancing
+		};
+		drawingSettings.SetShaderPassName(1, litShaderTagId);
+
+		var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+		context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);		
+		
+		// skybox
+		context.DrawSkybox(camera);
+		
+		// trasparent
+		sortingSettings.criteria = SortingCriteria.CommonTransparent;
+		drawingSettings.sortingSettings = sortingSettings;
+		filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+		context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+	}
+
+	void Setup()
+	{
+		context.SetupCameraProperties(camera);
+		CameraClearFlags flags = camera.clearFlags;
+		buffer.ClearRenderTarget(flags <= CameraClearFlags.Depth, 
+			flags <= CameraClearFlags.Color, 
+			flags == CameraClearFlags.Color ? camera.backgroundColor.linear : Color.clear);
+		buffer.BeginSample(SampleName);
+		ExecuteBuffer();
+		
+	}
+
+	void Submit()
+	{
+		buffer.EndSample(SampleName);
+		ExecuteBuffer();
+
+		context.Submit();
+	}
+	
+	void ExecuteBuffer () {
+		context.ExecuteCommandBuffer(buffer);
+		buffer.Clear();
+	}
+
+	bool Cull ()
+	{
+		if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+		{
+			cullingResults = context.Cull(ref p);
+			return true;
+		}
+		return false;
+	}
+
+}
